aea70a9278a2ce5bda1b0b5755e27a36a40cdaaa
Neal <nrjohnso@mtu.edu>
2013-03-22 02:58:46 -0400

    This is a (hopefully) demo ready version of the UI. Comments are on the way. Also, the package named Andrews I got from Andrew, but I realize a lot of it is Mac's as well.

---
+++ MapTool/UserInterface/ChatWindow.java
@@ -0,0 +1,26 @@
+import java.awt.Toolkit;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.gui.GUIContext;
+import org.newdawn.slick.gui.TextField;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class ChatWindow {
+
+	private String[] visibleChat;
+
+
+	public ChatWindow(Graphics g, int width, int x, int y){
+		int h = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 2*y);
+		//draw border
+		g.setColor(Color.lightGray);
+		g.fillRect(x-2, y-2, width + 4, h + 4);
+		//find height, draw the rectangle
+		g.setColor(Color.white);
+		g.fillRect(x, y, width, h);
+
+	}
+}
---
+++ MapTool/UserInterface/Menu.java
@@ -0,0 +1,38 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class Menu extends BasicGameState{
+
+	public Menu(int state){
+		
+	}
+
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+
+		
+	}
+
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getID() {
+		return 0;
+	}
+
+}
